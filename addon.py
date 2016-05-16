d48953c7f08bedb38e856aad0f417df132d1f023
Wintermute0110 <wintermute0110@gmail.com>
2016-05-16 23:39:53 +0900

    Declare UFT-8 and write copyright.

--- addon.py
+++ addon.py
@@ -1,7 +1,11 @@
-﻿#
+﻿# -*- coding: utf-8 -*-
+#
 # Advanced Emulator Launcher main script file
 #
 
+# Copyright (c) 2016 Wintermute0110 <wintermute0110@gmail.com>
+# Portions (c) 2010-2015 Angelscry and others
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; version 2 of the License.
