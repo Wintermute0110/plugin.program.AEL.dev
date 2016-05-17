814c5eb82645a3e1cea4a653a2256e0c2ea3966c
Wintermute0110 <wintermute0110@gmail.com>
2016-05-17 21:29:34 +0900

    Merge categories from catver.ini

--- util_MAME_XML_to_simple_XML.py
+++ util_MAME_XML_to_simple_XML.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-#
+# -*- coding: utf-8 -*-
 # Convert MAME output XML into a simplified XML suitable for offline scrapping.
-# (c) Wintermute0110 <wintermute0110@gmail.com> 2016
+#
+
+# Copyright (c) 2016 Wintermute0110 <wintermute0110@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,20 +14,75 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
+# --- INSTRUCTIONS -------------------------------------------------------------
 # To create the MAME XML file type
-#   $ mame -listxml > mame-xxxx.xml
-# where xxxx is the mame version, 0.173 will be 0173. Then, run this
-# utility,
-#   $ ./util_simpleXML_from_MAME_XML mame-xxxx.xml
+#
+# $ mame -listxml > mame_xxxx.xml
+#
+# where xxxx is the mame version, 0.173 will be 0173. Then, run this utility,
+#
+# $ ./util_simpleXML_from_MAME_XML
+#
 # The output file will be named MAME.xml
 
-# TODO Use catver.ini and possibly other MAME DAT files to get a much better
-#      output.
+# Download Catver.ini from ProgrettoSnaps: 
+# [Web]   http://www.progettosnaps.net/catver/
+# [File]  http://www.progettosnaps.net/catver/packs/pS_CatVer.zip
+# ------------------------------------------------------------------------------
 
-# Python standard library
+# --- Configuration -----------------------------------------------------------
+MAME_XML_filename   = 'mame_0173.xml'
+Catver_ini_filename = 'catver.ini'
+
+# --- Python standard library ---
 import xml.etree.ElementTree as ET 
 import re
 import sys
+
+# -----------------------------------------------------------------------------
+# Load catver.ini
+# -----------------------------------------------------------------------------
+print('Parsing ' + Catver_ini_filename)
+categories_dic = {}
+categories_set = set()
+__debug_do_list_categories = False
+read_status = 0
+try:
+    # read_status FSM values
+    # 0 -> Looking for '[Category]' tag
+    # 1 -> Reading categories
+    # 2 -> Categories finished. STOP
+    f = open(Catver_ini_filename, 'rt')
+    for cat_line in f:
+        stripped_line = cat_line.strip()
+        if __debug_do_list_categories: print('Line "' + stripped_line + '"')
+        if read_status == 0:
+            if stripped_line == '[Category]':
+                if __debug_do_list_categories: print('Found [Category]')
+                read_status = 1
+        elif read_status == 1:
+            line_list = stripped_line.split("=")
+            if len(line_list) == 1:
+                read_status = 2
+                continue
+            else:
+                if __debug_do_list_categories: print(line_list)
+                machine_name = line_list[0]
+                category = line_list[1]
+                if machine_name not in categories_dic:
+                    categories_dic[machine_name] = category
+                categories_set.add(category)
+        elif read_status == 2:
+            print('Reached end of categories parsing.')
+            break
+        else:
+            print('Unknown read_status FSM value. Aborting.')
+            sys.exit(10)
+    f.close()
+except:
+    pass
+print('Catver Number of machines   {:6d}'.format(len(categories_dic)))
+print('Catver Number of categories {:6d}'.format(len(categories_set)))
 
 # -----------------------------------------------------------------------------
 # Incremental Parsing approach B (from [1])
@@ -34,7 +91,7 @@ import sys
 # grab only the information we want and discard the rest.
 # See http://effbot.org/zone/element-iterparse.htm [1]
 __debug_MAME_XML_parser = 0
-context = ET.iterparse('mame_0173.xml', events=("start", "end"))
+context = ET.iterparse(MAME_XML_filename, events=("start", "end"))
 context = iter(context)
 event, root = context.next()
 
@@ -88,16 +145,15 @@ for event, elem in context:
         elem.clear()
         machines[machine_name] = machine
 
-    num_iteration += 1
-    # --- Stop after some iteration for debug ---
-    # if num_iteration > 10000:
-    #    break
-
     # --- Print something to prove we are doing stuff ---
+    num_iteration += 1
     if num_iteration % 25000 == 0:
-      print('Processed {0:10d} events ({1:6d} machines so far) ...'.format(num_iteration, num_machines))
-print('Processed {0} MAME XML events'.format(num_iteration))
-print('Total number of machines {0}'.format(num_machines))
+      print('Processed {:10d} events ({:6d} machines so far) ...'.format(num_iteration, num_machines))
+
+    # --- Stop after some iterations for debug ---
+    # if num_iteration > 25000: break
+print('Processed {} MAME XML events'.format(num_iteration))
+print('Total number of machines {}'.format(num_machines))
 
 # -----------------------------------------------------------------------------
 # Now write simplified XML output file
@@ -108,23 +164,25 @@ def string_to_XML(str):
 
     return str_A.replace('<', '&lt;').replace('>', '&gt;')
 
-# log_info('_fs_write_Favourites_XML_file() Saving XML file {0}'.format(roms_xml_file))
+# log_info('_fs_write_Favourites_XML_file() Saving XML file {}'.format(roms_xml_file))
 try:
     str_list = []
     str_list.append('<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n')
     str_list.append('<menu>\n')
     for key in sorted(machines):
-        name = string_to_XML(machines[key]['name'])
-        description = string_to_XML(machines[key]['description'])
-        year = string_to_XML(machines[key]['year'])
+        name         = string_to_XML(machines[key]['name'])
+        description  = string_to_XML(machines[key]['description'])
+        year         = string_to_XML(machines[key]['year'])
         manufacturer = string_to_XML(machines[key]['manufacturer'])
+        genre        = categories_dic[key] if key in categories_dic else 'Unknown'
+        genre        = string_to_XML(genre)
         str_list.append('<game name="{0}">\n'.format(name) +
                         '  <description>'  + description   + '</description>\n' +
                         '  <year>'         + year          + '</year>\n' +
                         '  <manufacturer>' + manufacturer  + '</manufacturer>\n' +
+                        '  <genre>'        + genre         + '</genre>\n' +
                         '</game>\n')
     str_list.append('</menu>\n')
-
     file_obj = open('MAME.xml', 'wt' )
     file_obj.write(''.join(str_list)) 
     file_obj.close()
