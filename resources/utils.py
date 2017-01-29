22c86b0734536600baa838a27fa7b315afa158c9
Wintermute0110 <wintermute0110@gmail.com>
2017-01-29 21:07:42 +0900

    Multidisc ROM information parsing working OK.

--- resources/utils.py
+++ resources/utils.py
@@ -215,48 +215,94 @@ def text_format_ROM_title(title, clean_tags):
 # -------------------------------------------------------------------------------------------------
 # Multidisc ROM support
 # -------------------------------------------------------------------------------------------------
+def text_get_ROM_basename_tokens(basename_str):
+    DEBUG_TOKEN_PARSER = False
+
+    # --- Parse ROM base_noext/basename_str into tokens ---
+    reg_exp = '\[.+?\]|\(.+?\)|\{.+?\}|[^\[\(\{]+'
+    tokens_raw = re.findall(reg_exp, basename_str)
+    if DEBUG_TOKEN_PARSER:
+        log_debug('text_get_ROM_basename_tokens() tokens_raw   {0}'.format(tokens_raw))
+
+    # >> Strip tokens
+    tokens_strip = list()
+    for token in tokens_raw: tokens_strip.append(token.strip())
+    if DEBUG_TOKEN_PARSER:
+        log_debug('text_get_ROM_basename_tokens() tokens_strip {0}'.format(tokens_strip))
+
+    # >> Remove empty tokens ''
+    tokens_clean = list()
+    for token in tokens_strip: 
+        if token: tokens_clean.append(token)
+    if DEBUG_TOKEN_PARSER:        
+        log_debug('text_get_ROM_basename_tokens() tokens_clean {0}'.format(tokens_clean))
+
+    # >> Remove '-' tokens from Trurip multidisc names
+    tokens = list()
+    for token in tokens_clean:
+        if token == '-': continue
+        tokens.append(token)
+    if DEBUG_TOKEN_PARSER:
+        log_debug('text_get_ROM_basename_tokens() tokens       {0}'.format(tokens))
+
+    return tokens
+
 class MultiDiscInfo:
     def __init__(self, ROM_FN):
-        self.ROM_FN = ROM_FN
+        self.ROM_FN      = ROM_FN
         self.isMultiDisc = False
-        self.setName = ''
-        self.order = 0
+        self.setName     = ''
+        self.extension   = ROM_FN.getExt()
+        self.order       = 0
 
 def text_get_multidisc_info(ROM_FN):
     MDSet = MultiDiscInfo(ROM_FN)
     
     # --- Parse ROM base_noext into tokens ---
-    base_noext = ROM_FN.getBase_noext()
-    reg_exp = '\[.+?\]\s?|\(.+?\)\s?|\{.+?\}|[^\[\(\{]+|- \(.+?\)\s?'
-    tokens = re.findall(reg_exp, base_noext)
-    log_debug('text_get_multidisc_info() tokens = {0}'.format(tokens))
+    tokens = text_get_ROM_basename_tokens(ROM_FN.getBase_noext())
 
     # --- Check if ROM belongs to a multidisc set and get set name and order ---
     # Algortihm:
     # 1) Iterate list of tokens
     # 2) If a token marks a multidisk ROM extract set order
     # 3) Define the set basename by removing the multidisk token
+    MultDiscFound = False
     for index, token in enumerate(tokens):
+        # --- Redump ---
         matchObj = re.match(r'\(Disc ([0-9]+)\)', token)
         if matchObj:
-            log_debug('text_get_multidisc_info() Matched Redump multidisc ROM')
+            log_debug('text_get_multidisc_info() ### Matched Redump multidisc ROM ###')
+            tokens_idx = range(0, len(tokens))
+            tokens_idx.remove(index)
+            tokens_nodisc_idx = list(tokens_idx)
+            tokens_mdisc = [tokens[x] for x in tokens_nodisc_idx]
+            MultDiscFound = True
             break
 
+        # --- TOSEC/Trurip ---
         matchObj = re.match(r'\(Disc ([0-9]+) of ([0-9]+)\)', token)
         if matchObj:
-            log_debug('text_get_multidisc_info() Matched TOSEC multidisc ROM')
+            log_debug('text_get_multidisc_info() ### Matched TOSEC/Trurip multidisc ROM ###')
             tokens_idx = range(0, len(tokens))
-            tokens_nodisc_idx = tokens_idx.remove(index)
-            tokens_nodisc_list = [tokens[x] for x in tokens_nodisc_idx]
-            log_debug('text_get_multidisc_info() tokens_idx         = {0}'.format(tokens_idx))
-            log_debug('text_get_multidisc_info() tokens_nodisc_idx  = {0}'.format(tokens_nodisc_idx))
-            log_debug('text_get_multidisc_info() tokens_nodisc_list = {0}'.format(tokens_nodisc_list))
-            MDSet.isMultiDisc = True
-            MDSet.setName = ' '.join(tokens_nodisc_list)
-            MDSet.order = int(matchObj.group(1))
-            log_debug('text_get_multidisc_info() setName  = "{0}"'.format(MDSet.setName))
-            log_debug('text_get_multidisc_info() order    = {0}'.format(MDSet.order))
+            tokens_idx.remove(index)
+            tokens_nodisc_idx = list(tokens_idx)
+            # log_debug('text_get_multidisc_info() tokens_idx         = {0}'.format(tokens_idx))
+            # log_debug('text_get_multidisc_info() index              = {0}'.format(index))
+            # log_debug('text_get_multidisc_info() tokens_nodisc_idx  = {0}'.format(tokens_nodisc_idx))
+            tokens_mdisc = [tokens[x] for x in tokens_nodisc_idx]
+            MultDiscFound = True
             break
+
+    if MultDiscFound:
+        MDSet.isMultiDisc = True
+        MDSet.setName = ' '.join(tokens_mdisc)
+        MDSet.order = int(matchObj.group(1))
+        log_debug('text_get_multidisc_info() base_noext   "{0}"'.format(ROM_FN.getBase_noext()))
+        log_debug('text_get_multidisc_info() tokens       {0}'.format(tokens))
+        log_debug('text_get_multidisc_info() tokens_mdisc {0}'.format(tokens_mdisc))
+        log_debug('text_get_multidisc_info() setName      "{0}"'.format(MDSet.setName))
+        log_debug('text_get_multidisc_info() extension    "{0}"'.format(MDSet.extension))
+        log_debug('text_get_multidisc_info() order        {0}'.format(MDSet.order))
 
     return MDSet
 
