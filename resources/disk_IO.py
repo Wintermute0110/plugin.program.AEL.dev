4b2963738fd5dd2f8a39cacf88573215e5e287ce
Chris Jungerius <cjungerius@olbico.nl>
2017-07-02 15:46:10 +0200

    removed left over test code

--- resources/disk_IO.py
+++ resources/disk_IO.py
@@ -1764,8 +1764,6 @@ def fs_import_category_NFO(nfo_FileName, categories, categoryID):
     
     log_debug('fs_import_category_NFO() is file "{0}"'.format(nfo_FileName.isfile()))
     log_debug('fs_import_category_NFO() exists "{0}"'.format(nfo_FileName.exists()))
-    mode = nfo_FileName.stat().st_mode()
-    isadir = not ISDIR(mode)
     log_debug('fs_import_category_NFO() is file stat "{0}"'.format(isadir))
 
     # --- Import data ---
