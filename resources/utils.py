dbeff807c3e483459ac30b5ef3c5038296205d52
Wintermute0110 <wintermute0110@gmail.com>
2018-10-16 15:23:36 +0900

    Move cryptography stuff into utils.py

--- resources/utils.py
+++ resources/utils.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-# Advanced Emulator Launcher
+# Advanced Emulator Launcher utilities and misc functions.
+#
+
 # Copyright (c) 2016-2018 Wintermute0110 <wintermute0110@gmail.com>
 # Portions (c) 2010-2015 Angelscry and others
 #
@@ -13,13 +15,24 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-# -------------------------------------------------------------------------------------------------
-# Utility functions which does not depend on Kodi modules (except log_* functions)
-# -------------------------------------------------------------------------------------------------
+# --- Module documentation ---
+# 1. This function contains utilities that do not depend on Kodi modules and utilities that
+#    depend on Kodi modules. Appropiate replacements are provided when the Kodi modules are
+#    not available, for example when running this file with the standard Python interpreter and
+#    not Kodi Python interpreter.
+#
+# 2. Filesystem access utilities are located in this file. Filesystem can use the Python
+#    standard library or Kodi Virtual FileSystem library if available.
+#
+# 3. utils.py must not depend on any other AEL module to avoid circular dependencies, with the
+#    exception of constants.py
+#
+# 4. Functions starting with _ are internal module functions not to be called externally.
+#
+
 # --- Python compiler flags ---
 from __future__ import unicode_literals
-
-# --- Python standard library ---
+from __future__ import division
 import sys
 import os
 import shutil
@@ -32,49 +45,45 @@ import string
 import fnmatch
 import HTMLParser
 
+# --- AEL modules ---
 from constants import *
 from filename import *
 from utils_kodi import *
 
+# #################################################################################################
+# #################################################################################################
+# Standard Python utilities
+# #################################################################################################
+# #################################################################################################
+
+# -------------------------------------------------------------------------------------------------
 # OS utils
-# >> Determine platform
-# >> See http://stackoverflow.com/questions/446209/possible-values-from-sys-platform
-def is_windows():    
-    return sys.platform == 'win32' or sys.platform == 'win64' or sys.platform == 'cygwin'
+# -------------------------------------------------------------------------------------------------
+# Determine interpreter running platform
+# See http://stackoverflow.com/questions/446209/possible-values-from-sys-platform
 
-def is_osx():
-    return sys.platform.startswith('darwin')
-
-def is_linux():
-     return sys.platform.startswith('linux') and not is_android()
-
-def is_android():
-    if not sys.platform.startswith('linux'):
-        return False
-    
+# --- Determine the platform and cache all possible values for maximum speed ---
+cached_sys_platform = sys.platform
+def _aux_is_android():
+    if not cached_sys_platform.startswith('linux'): return False
     return 'ANDROID_ROOT' in os.environ or 'ANDROID_DATA' in os.environ or 'XBMC_ANDROID_APK' in os.environ
 
-# --- AEL modules ---
-# >> utils.py must not depend on any other AEL module to avoid circular dependencies.
+is_windows_bool = cached_sys_platform == 'win32' or cached_sys_platform == 'win64' or cached_sys_platform == 'cygwin'
+is_osx_bool     = cached_sys_platform.startswith('darwin')
+is_android_bool = _aux_is_android()
+is_linux_bool   = cached_sys_platform.startswith('linux') and not is_android_bool
 
-
-# OS utils
-# >> Determine platform
-# >> See http://stackoverflow.com/questions/446209/possible-values-from-sys-platform
-def is_windows():    
-    return sys.platform == 'win32' or sys.platform == 'win64' or sys.platform == 'cygwin'
+def is_windows():
+    return is_windows_bool
 
 def is_osx():
-    return sys.platform.startswith('darwin')
+    return is_osx_bool
 
 def is_linux():
-     return sys.platform.startswith('linux') and not is_android()
+    return is_android_bool
 
 def is_android():
-    if not sys.platform.startswith('linux'):
-        return False
-    
-    return 'ANDROID_ROOT' in os.environ or 'ANDROID_DATA' in os.environ or 'XBMC_ANDROID_APK' in os.environ
+    return is_linux_bool
 
 # -------------------------------------------------------------------------------------------------
 # Strings and text
@@ -83,10 +92,10 @@ def is_android():
 # If max_length == -1 do nothing (no length limit).
 #
 def text_limit_string(string, max_length):
-  if max_length > 5 and len(string) > max_length:
-    string = string[0:max_length-3] + '...'
+    if max_length > 5 and len(string) > max_length:
+        string = string[0:max_length-3] + '...'
 
-  return string
+    return string
 
 #
 # Given a Category/Launcher name clean it so the cleaned srt can be used as a filename.
@@ -592,3 +601,188 @@ def print_game_image_list(scraperObj, results, asset_kind):
             url           = text_limit_string(image['URL'], URL_LENGTH)
             print("{0} {1} {2}".format(display_name.ljust(NAME_LENGTH), id.ljust(ID_LENGTH), url.ljust(URL_LENGTH)))
         print('\n')
+
+# #################################################################################################
+# #################################################################################################
+# Cryptographic utilities
+# #################################################################################################
+# #################################################################################################
+
+# NOTE OpenSSL library will be included in Kodi M****
+
+# --- Python standard library ---
+# NOTE binascii must not be used! See https://docs.python.org/2/library/binascii.html
+import binascii
+from base64 import b64decode
+from base64 import b64encode
+from datetime import datetime, timedelta
+
+try:
+    from OpenSSL import crypto, SSL
+    UTILS_OPENSSL_AVAILABLE = True
+except:
+    UTILS_OPENSSL_AVAILABLE = False
+
+try:
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives import serialization
+    UTILS_CRYPTOGRAPHY_AVAILABLE = True
+except:
+    UTILS_CRYPTOGRAPHY_AVAILABLE = False
+
+try:
+    from Crypto.PublicKey import RSA
+    from Crypto.Signature import PKCS1_v1_5
+    from Crypto.Hash import SHA256
+    from Crypto.Cipher import AES
+    from Crypto.Random import get_random_bytes
+    UTILS_PYCRYPTO_AVAILABLE = True
+except:
+    UTILS_PYCRYPTO_AVAILABLE = False
+
+#
+# Creates a new self signed certificate base on OpenSSL PEM format.
+# cert_name: the CN value of the certificate
+# cert_file_path: the path to the .crt file of this certificate
+# key_file_paht: the path to the .key file of this certificate
+#
+def create_self_signed_cert(cert_name, cert_file_path, key_file_path):
+    # create a key pair
+    k = crypto.PKey()
+    k.generate_key(crypto.TYPE_RSA, 2048)
+
+    now    = datetime.now()
+    expire = now + timedelta(days=365)
+
+    # create a self-signed cert
+    cert = crypto.X509()
+    cert.get_subject().C = "GL"
+    cert.get_subject().ST = "GL"
+    cert.get_subject().L = "Kodi"
+    cert.get_subject().O = "ael"
+    cert.get_subject().OU = "ael"
+    cert.get_subject().CN = cert_name
+    cert.set_serial_number(1000)
+    cert.set_notBefore(now.strftime("%Y%m%d%H%M%SZ").encode())
+    cert.set_notAfter(expire.strftime("%Y%m%d%H%M%SZ").encode())
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(k)
+    cert.sign(k, 'sha1')
+
+    log_debug('Creating certificate file {0}'.format(cert_file_path.getOriginalPath()))
+    data = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
+    cert_file_path.writeAll(data, 'wt')
+
+    log_debug('Creating certificate key file {0}'.format(key_file_path.getOriginalPath()))
+    data = crypto.dump_privatekey(crypto.FILETYPE_PEM, k)
+    key_file_path.writeAll(data, 'wt')
+
+def getCertificatePublicKeyBytes(certificate_data):
+    pk_data = getCertificatePublicKey(certificate_data)
+    return bytearray(pk_data)
+
+def getCertificatePublicKey(certificate_data):
+    cert = crypto.x509.load_pem_x509_certificate(certificate_data, default_backend())
+    pk = cert.public_key()
+    pk_data = pk.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+    return pk_data
+
+def getCertificateSignature(certificate_data):
+    cert = crypto.x509.load_pem_x509_certificate(certificate_data, default_backend())
+    return cert.signature
+
+def verify_signature(data, signature, certificate_data):
+    pk_data = getCertificatePublicKey(certificate_data)
+    rsakey = RSA.importKey(pk_data) 
+    signer = PKCS1_v1_5.new(rsakey) 
+
+    digest = SHA256.new() 
+    digest.update(data)
+
+    if signer.verify(digest, signature):
+        return True
+
+    return False
+
+def sign_data(data, key_certificate):
+    rsakey = RSA.importKey(key_certificate) 
+    signer = PKCS1_v1_5.new(rsakey) 
+    digest = SHA256.new() 
+        
+    digest.update(data) 
+    sign = signer.sign(digest) 
+
+    return sign
+
+def randomBytes(size):
+    return get_random_bytes(size)
+
+class HashAlgorithm(object):
+    def __init__(self, shaVersion):
+        self.shaVersion = shaVersion
+        if self.shaVersion == 256:
+            self.hashLength = 32
+        else:
+            self.hashLength = 20
+       
+    def _algorithm(self):
+
+        if self.shaVersion == 256:
+            return hashlib.sha256()
+        else:
+            return hashlib.sha1()
+
+    def hash(self, value):
+        algorithm = self._algorithm()
+        algorithm.update(value)
+        hashedValue = algorithm.digest()
+        return hashedValue
+
+    def hashToHex(self, value):
+        hashedValue = self.hash(value)
+        return binascii.hexlify(hashedValue)
+
+    def digest_size(self):
+        return self.hashLength
+
+# Block size in bytes.
+BLOCK_SIZE = 16
+
+class AESCipher(object):
+
+    def __init__(self, key, hashAlgorithm):
+        
+        keyHashed = hashAlgorithm.hash(key)
+        truncatedKeyHashed = keyHashed[:16]
+
+        self.key = truncatedKeyHashed
+
+    def encrypt(self, raw):
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        encrypted = cipher.encrypt(raw)
+        return encrypted
+
+    def encryptToHex(self, raw):
+        encrypted = self.encrypt(raw)
+        return binascii.hexlify(encrypted)
+
+    def decrypt(self, enc):
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        decrypted = cipher.decrypt(str(enc))
+        return decrypted
+
+# #################################################################################################
+# #################################################################################################
+# Filesystem utilities
+# #################################################################################################
+# #################################################################################################
+
+
+# #################################################################################################
+# #################################################################################################
+# Kodi utilities
+# #################################################################################################
+# #################################################################################################
