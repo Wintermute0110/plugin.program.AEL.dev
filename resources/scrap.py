734b035b0e43a4f4ad7c9b81c24b954160213644
Wintermute0110 <wintermute0110@gmail.com>
2019-06-27 20:51:12 +0900

    Update TODO.

--- resources/scrap.py
+++ resources/scrap.py
@@ -3,6 +3,10 @@
 # Advanced Emulator Launcher scraping engine.
 # MAKE THIS CODE AS 0.10.0 READY AS POSSIBLE.
 
+# --- Information about scraping ---
+# https://github.com/muldjord/skyscraper
+# https://github.com/muldjord/skyscraper/blob/master/docs/SCRAPINGMODULES.md
+
 # Copyright (c) 2016-2019 Wintermute0110 <wintermute0110@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
