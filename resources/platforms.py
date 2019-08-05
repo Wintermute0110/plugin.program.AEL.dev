06114155874e1b8c59e327f6dae9264832d6ca21
Wintermute0110 <wintermute0110@gmail.com>
2019-08-05 19:07:58 +0900

    Updated file header.

--- resources/platforms.py
+++ resources/platforms.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-#
-# Advanced Emulator Launcher platform and emulator information
-#
 
-# Copyright (c) 2016-2017 Wintermute0110 <wintermute0110@gmail.com>
+# Advanced Emulator Launcher platform and emulator information.
+
+# Copyright (c) 2016-2019 Wintermute0110 <wintermute0110@gmail.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -11,8 +10,8 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
 
 # --- Python standard library ---
 from __future__ import unicode_literals
