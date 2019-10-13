5325e21492755a11bab0a14f4adbe835763c09f3
Wintermute0110 <wintermute0110@gmail.com>
2019-10-13 17:01:57 +0900

    Parser working OK.

--- dev-core/parse_Libretro_info_files.py
+++ dev-core/parse_Libretro_info_files.py
@@ -5,7 +5,11 @@
 
 # --- Python standard library ---
 from __future__ import unicode_literals
+import glob
+import json
 import os
+import pprint
+import re
 import sys
 
 # --- AEL modules ---
@@ -19,7 +23,7 @@ if __name__ == "__main__" and __package__ is None:
 # --- functions ----------------------------------------------------------------------------------
 
 # --- configuration ------------------------------------------------------------------------------
-fname_output = 'data/Libretro_info.json'
+json_fname = 'data/Libretro_info.json'
 
 # --- main ---------------------------------------------------------------------------------------
 INFO_KEYWORDS = [
@@ -32,6 +36,7 @@ INFO_KEYWORDS = [
     'systemname',
     'systemid',
     'database',
+    'database_match_archive_member',
     'license',
     'permissions',
     'display_version',
@@ -47,4 +52,52 @@ INFO_KEYWORDS_SPECIAL = [
     'firmwareN_opt', # N is a number.
 ]
 
+json_data = {}
+
 # --- Get list of info files. Ignore 00_example_libretro.info ---
+file_list = [f for f in glob.glob('info/*.info')]
+for f in sorted(file_list):
+    # Skip some files.
+    if f == 'info/00_example_libretro.info': continue
+    json_data[f] = {}
+    for keyword in INFO_KEYWORDS: json_data[f][keyword] = ''
+
+    # Read file line by line.
+    print('\nProcessing "{}"'.format(f))
+    fp = open(f, 'r')
+    for line in fp:
+        line = line.strip().decode('utf-8')
+        if line == '': continue
+        if line[0] == '#': continue
+        # print('Line "{}"'.format(line))
+
+        # Parse line display_name = "Atari - 5200 (Atari800)"
+        m = re.search('^([\w]+)\s+=\s+"(.*)"$', line)
+        if m:
+            print('Match A: "{}" "{}"'.format(m.group(1), m.group(2)))
+            keyword, value = m.group(1), m.group(2)
+            # Special firmware keywords
+            mf = re.search('^firmware(\d+)_(\w+)$', keyword)
+            if mf:
+                print('Skipping keyword "{}"'.format(keyword))
+                continue
+            # Standard keyword
+            if keyword not in INFO_KEYWORDS: raise ValueError(keyword)
+            json_data[f][keyword] = value
+            continue
+
+        # Parse line firmware_count = 1
+        m = re.search('^([\w]+)\s+=\s+(\d*)$', line)
+        if m:
+            print('Match A: "{}" "{}"'.format(m.group(1), m.group(2)))
+            keyword, number = m.group(1), m.group(2)
+            json_data[f][keyword] = number
+            continue
+
+        # If we reach this point there is an error in the info file.
+        raise ValueError(line)
+    fp.close()
+
+# Save output JSON.
+with open(json_fname, 'w') as outfile:
+    outfile.write(json.dumps(json_data, sort_keys = True, indent = 4))
