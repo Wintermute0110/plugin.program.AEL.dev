ef62ebd181cf05a43239bd8f3b33b100838b267c
Wintermute0110 <wintermute0110@gmail.com>
2019-10-25 20:08:04 +0900

    Also parse BIOS data.

--- dev-core/parse_Libretro_info_files.py
+++ dev-core/parse_Libretro_info_files.py
@@ -60,36 +60,47 @@ for f in sorted(file_list):
     # Skip some files.
     if f == 'info/00_example_libretro.info': continue
     json_data[f] = {}
-    for keyword in INFO_KEYWORDS: json_data[f][keyword] = ''
+    for keyword in INFO_KEYWORDS: 
+        if keyword == 'notes':
+            json_data[f][keyword] = []
+        else:
+            json_data[f][keyword] = ''
 
+    # --- First pass ---
     # Read file line by line.
-    print('\nProcessing "{}"'.format(f))
+    print('\nFirst pass "{}"'.format(f))
     fp = open(f, 'r')
     for line in fp:
         line = line.strip().decode('utf-8')
         if line == '': continue
         if line[0] == '#': continue
-        # print('Line "{}"'.format(line))
+        # print("Line '{}'".format(line))
 
-        # Parse line display_name = "Atari - 5200 (Atari800)"
-        m = re.search('^([\w]+)\s+=\s+"(.*)"$', line)
+        # Parse lines 'firmware0_desc = "panafz1.bin (Panasonic FZ-1 BIOS)" '
+        m = re.search('^firmware(\d+)_(\w+)\s+=\s+"(.*)"$$', line)
         if m:
-            print('Match A: "{}" "{}"'.format(m.group(1), m.group(2)))
-            keyword, value = m.group(1), m.group(2)
-            # Special firmware keywords
-            mf = re.search('^firmware(\d+)_(\w+)$', keyword)
-            if mf:
-                print('Skipping keyword "{}"'.format(keyword))
-                continue
-            # Standard keyword
-            if keyword not in INFO_KEYWORDS: raise ValueError(keyword)
-            json_data[f][keyword] = value
+            print('Match A: "{}" "{}" "{}". Skipping.'.format(m.group(1), m.group(2), m.group(3)))
             continue
 
-        # Parse line firmware_count = 1
+        # Parse lines 'display_name = "Atari - 5200 (Atari800)"'
+        m = re.search('^([\w]+)\s+=\s+"(.*)"$', line)
+        if m:
+            print('Match B: "{}" "{}"'.format(m.group(1), m.group(2)))
+            keyword, value = m.group(1), m.group(2)
+            # Standard keyword
+            if keyword not in INFO_KEYWORDS:
+                print("Line '{}'".format(line))
+                raise ValueError(keyword)
+            if keyword == 'notes':
+                json_data[f][keyword].append(value)
+            else:
+                json_data[f][keyword] = value
+            continue
+
+        # Parse lines 'firmware_count = 1'
         m = re.search('^([\w]+)\s+=\s+(\d*)$', line)
         if m:
-            print('Match A: "{}" "{}"'.format(m.group(1), m.group(2)))
+            print('Match C: "{}" "{}"'.format(m.group(1), m.group(2)))
             keyword, number = m.group(1), m.group(2)
             json_data[f][keyword] = number
             continue
@@ -97,6 +108,37 @@ for f in sorted(file_list):
         # If we reach this point there is an error in the info file.
         raise ValueError(line)
     fp.close()
+
+    # --- Second pass ---
+    # Create list of BIOSes
+    BIOS_list = []
+    if json_data[f]['firmware_count']:
+        num_BIOSes = int(json_data[f]['firmware_count'])
+    else:
+        print('firmware_count not defined. Core has no BIOSes')
+        json_data[f]['BIOS'] = BIOS_list
+        continue
+    print('Core has {} BIOSes'.format(num_BIOSes))
+    for i in range(num_BIOSes):
+        BIOS_list.append({'desc' : '', 'path' : '', 'opt'  : '', 'md5'  : ''})
+    # pprint.pprint(BIOS_list)
+
+    print('\nSecond pass "{}"'.format(f))
+    fp = open(f, 'r')
+    for line in fp:
+        line = line.strip().decode('utf-8')
+        if line == '': continue
+        if line[0] == '#': continue
+
+        # Parse lines 'firmware0_desc = "panafz1.bin (Panasonic FZ-1 BIOS)" '
+        m = re.search('^firmware(\d+)_(\w+)\s+=\s+"(.*)"$$', line)
+        if m:
+            print('Match A: "{}" "{}" "{}"'.format(m.group(1), m.group(2), m.group(3)))
+            b_index_str, keyword_str, value_str = m.group(1), m.group(2), m.group(3)
+            b_index = int(b_index_str)
+            BIOS_list[b_index][keyword_str] = value_str
+    fp.close()
+    json_data[f]['BIOS'] = BIOS_list
 
 # Save output JSON.
 with open(json_fname, 'w') as outfile:
